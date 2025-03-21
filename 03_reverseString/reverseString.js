748e2cd003dc9be9c753ba8dd6c6dec0a7deb216
pancracks <atharva6987@gmail.com>
2025-03-21 13:24:21 +0530

    3rd exercise completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+        let arr = string.split("")
+        let newarr = arr.reverse()
+        let result = ""
+        for (let s of newarr) {
+            result += s
+        }
+    return result
 
-};
+}
 
 // Do not edit below this line
 module.exports = reverseString;
