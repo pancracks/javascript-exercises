3c2c635038002a03ae170c41bd3c867b4c9cad8b
pancracks <atharva6987@gmail.com>
2025-03-24 12:53:56 +0530

    4th exercise done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    const newarr = arr.filter(x => !args.includes(x))
+    return newarr
 };
 
 // Do not edit below this line
