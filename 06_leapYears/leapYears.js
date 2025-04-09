24face985d6746862ebb26e7fe0c54f47f158b5d
pancracks <atharva6987@gmail.com>
2025-04-09 11:59:57 +0530

    6th done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,16 @@
-const leapYears = function() {
+const leapYears = function(leap) {
+    let divByFour = leap % 4 === 0;
+    let century = leap % 100 === 0;
+    let divByFourCen = leap % 400 === 0;
+
+    if (divByFour &&
+        (!century || divByFourCen)
+    ){
+        return true
+    }
+    else {
+        return false
+    }
 
 };
 
