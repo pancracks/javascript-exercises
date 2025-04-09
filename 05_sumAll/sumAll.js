0b4afd6b62336dc47be7c052625d2d55d4effeae
pancracks <atharva6987@gmail.com>
2025-04-09 11:36:59 +0530

    5th excercise done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,17 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+    if (typeof num1 != "number" || typeof num2 != "number" || num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)){
+        return "ERROR"
+    }
+    let start = Math.min(num1,num2);
+    let end = Math.max(num1,num2);
+    let sum = 0;
+
+    for (i = start; i <= end; i++){
+         sum += i
+    }
+    return sum
+
 
 };
 
